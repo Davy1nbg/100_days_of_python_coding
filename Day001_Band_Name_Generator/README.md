e81bb7212efd98bc8240139b8c59286fbe7e75b2
Davy1nbg <david.pierzyna@mailbox.org>
2025-07-24 20:13:26 +0200

    Day 1

---
+++ Day001_Band_Name_Generator/README.md
@@ -0,0 +1,3 @@
+The code files were created as part of this completing course: 100 Days of Code: The Complete Python Pro Bootcamp. https://www.udemy.com/course/100-days-of-code
+
+Day 1 - Working with Variables in Python to Manage Data
---
+++ Day001_Band_Name_Generator/main.py
@@ -0,0 +1,10 @@
+# The code files were created as part of completing this course: 
+# 100 Days of Code: The Complete Python Pro Bootcamp. 
+# https://www.udemy.com/course/100-days-of-code
+
+# Day 1 - Working with Variables in Python to Manage Data
+
+print("Welcome to the BNG (Band Name Generator")
+city = input("In which city did you grow up?\n>>")
+pet = input("What is the name of your pet?\n>>")
+print(f"Your band name could be: {city} {pet}")
